f5fd6d29aea230c7ab70124558efe64c27278c7b
TarunInstaveritas <tarunshrivastva@instaveritas.com>
2020-06-14 17:57:59 +0530

    navbar andded successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from "@angular/common/http";
 import { NgxPaginationModule } from "ngx-pagination";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -77,6 +84,13 @@ import { JsonLdComponent } from './json-ld/json-ld.component';
     AppRoutingModule,
     HttpClientModule,
     NgxPaginationModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
